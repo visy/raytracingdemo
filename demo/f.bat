a522e7848f3210bc1ced339c167178cc2c808629
Visa-Valtteri Pimiä <vp79799@gmail.com>
2023-03-05 13:48:30 +0200

    joku

--- demo/f.bat
+++ demo/f.bat
@@ -1,2 +1,2 @@
-g++ demo.cpp pixie.cpp pixie_win.cpp device.c track.c -lwsock32 -lws2_32 -fpermissive -D DEMO_FULLSCREEN=true -lgdi32 -fopenmp -lbass -O3 -s -o demo.exe
+g++ demo.cpp pixie.cpp pixie_win.cpp device.c track.c -lwsock32 -lws2_32 -fpermissive -D DEMO_FULLSCREEN=true -lgdi32 -fopenmp -lbass -mwindows -O3 -s -o demo.exe
 demo.exe
